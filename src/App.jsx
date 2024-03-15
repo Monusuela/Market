3d26669d430de506951cb886ec3bed721b6ff42c
Mohinur <157112134+Monusuela@users.noreply.github.com>
2024-03-15 17:06:26 +0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import "./App.css"
 import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home";
 import Product from "./pages/Product"
+import Bag from "./pages/Bag";
 
 const App = () => {
   return (
     <Fragment>
       <main className="main">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/product/:id" element={<Product />} />
+          <Route path="/Market" index element={<Home />} />
+          <Route path="/Market/product/:id" element={<Product />} />
           <Route path="*" element={<h2 className="title-2">404</h2>} />
+          <Route path="/Market/bag" element={<Bag />} />
         </Routes>
       </main>
     </Fragment>
